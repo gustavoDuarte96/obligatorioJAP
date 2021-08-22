02daf5cc039a1d6c0258a9fcdf878879621ccf73
gustavoDuarte96 <gd245149@fi365.ort.edu.uy>
2021-08-22 11:25:21 -0300

    usuarios

--- js/login.js
+++ js/login.js
@@ -1,106 +1,106 @@
-window.addEventListener("load", inicio);
+window.addEventListener("load", modal);
 
-function inicio(){
-    modal();
-}
+function modal() {
+  const formulario = document.querySelector("#idFormulario");
 
-function modal(){
-    const formulario = document.querySelector('#idFormulario');
-    
-    formulario.addEventListener('submit', (e) => {
-        e.preventDefault();
-    
-        const registroUsuario = document.querySelector('#idUsuario').value;
-        const registroContraseña = document.querySelector('#idPassword').value;
-    
-        auth
-            .createUserWithEmailAndPassword(registroUsuario, registroContraseña)
-            .then(userCredential => {
-                localStorage.usuario = JSON.stringify(registroUsuario);
-                formulario.reset();
-                const divError = document.querySelector('#idErrores');
-                divError.innerHTML = "";
-                formulario.innerHTML = `<h3 style="color: green;">Te has registrado correctamente!</h3>`
-                setTimeout(() => {
-                    console.log("1 Segundo esperado")
-                  }, 1000);
-                window.location.replace("home.html");            
-            })
-            .catch(err => {
-                const divError = document.querySelector('#idErrores');
-                divError.innerHTML = "";
-                let esMail = false;
-                let contraseñaCorrecta = false;
-                for(let i = 0 ; i < registroUsuario.length ; i++){
-                    if(registroUsuario[i] == "@"){
-                        esMail = true;
-                    }
-                }
-                if(registroContraseña.length >= 8){
-                    contraseñaCorrecta = true;
-                }
-                if(!esMail || !contraseñaCorrecta){
-                    divError.innerHTML = `<p style="color: red;">Debe ingresar un email correcto y la contraseña debe superar los 8 digitos</p>`
-                    formulario.reset();
-                }else{
-                    divError.innerHTML = `<p style="color: red;">Este correo ya esta ingresado</p>`
-                    formulario.reset();
-                }
+  formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-            })
-    
-    })
+    const registroUsuario = document.querySelector("#idUsuario").value;
+    const registroContraseña = document.querySelector("#idPassword").value;
 
-    const googleSesion = document.querySelector('#iniciarGoogle');
-    googleSesion.addEventListener('click', e => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider)
-            .then(result => {
-                window.location.replace("home.html");
-            })
-            .catch(err => {
-            })
-    })
+    auth
+      .createUserWithEmailAndPassword(registroUsuario, registroContraseña)
+      .then((userCredential) => {
+        localStorage.usuario = JSON.stringify(registroUsuario);
+        formulario.reset();
+        const divError = document.querySelector("#idErrores");
+        divError.innerHTML = "";
+        formulario.innerHTML = `<h3 style="color: green;">Te has registrado correctamente!</h3>`;
+        setTimeout(() => {
+          console.log("1 Segundo esperado");
+        }, 1000);
+        window.location.replace("home.html");
+      })
+      .catch((err) => {
+        const divError = document.querySelector("#idErrores");
+        divError.innerHTML = "";
+        let esMail = false;
+        let contraseñaCorrecta = false;
+        for (let i = 0; i < registroUsuario.length; i++) {
+          if (registroUsuario[i] == "@") {
+            esMail = true;
+          }
+        }
+        if (registroContraseña.length >= 8) {
+          contraseñaCorrecta = true;
+        }
+        if (!esMail || !contraseñaCorrecta) {
+          divError.innerHTML = `<p style="color: red;">Debe ingresar un email correcto y la contraseña debe superar los 8 digitos</p>`;
+          formulario.reset();
+        } else {
+          divError.innerHTML = `<p style="color: red;">Este correo ya esta ingresado</p>`;
+          formulario.reset();
+        }
+      });
+  });
 
-    const facebookSesion = document.querySelector('#iniciarFacebook');
-    facebookSesion.addEventListener('click', e => {
-        const provider = new firebase.auth.FacebookAuthProvider();
-        auth.signInWithPopup(provider)
-            .then(result => {
-                window.location.replace("home.html");
-            })
-            .catch(err => {
-            })
-    })
+  const googleSesion = document.querySelector("#iniciarGoogle");
+  googleSesion.addEventListener("click", (e) => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        const registroUsuario = "Cuenta de Google";
+        localStorage.usuario = JSON.stringify(registroUsuario);
+        window.location.replace("home.html");
+      })
+      .catch((err) => {});
+  });
 
-    const githubSesion = document.querySelector('#iniciarGithub');
-    githubSesion.addEventListener('click', e => {
-        const provider = new firebase.auth.GithubAuthProvider();
-        auth.signInWithPopup(provider)
-            .then(result => {
-                window.location.replace("home.html");
-            })
-            .catch(err => {
-            })
-    })
+  const facebookSesion = document.querySelector("#iniciarFacebook");
+  facebookSesion.addEventListener("click", (e) => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        const registroUsuario = "Cuenta de Facebook";
+        localStorage.usuario = JSON.stringify(registroUsuario);
+        window.location.replace("home.html");
+      })
+      .catch((err) => {});
+  });
 
-    const formularioInicio = document.querySelector('#idFormularioCreado');
-    formularioInicio.addEventListener('submit', e => {
-        e.preventDefault();
-        const usuarioInit = document.querySelector('#idUsuarioCreado').value;
-        const passwordInit = document.querySelector('#idPasswordCreado').value;
+  const githubSesion = document.querySelector("#iniciarGithub");
+  githubSesion.addEventListener("click", (e) => {
+    const provider = new firebase.auth.GithubAuthProvider();
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        const registroUsuario = "Cuenta de GitHub";
+        localStorage.usuario = JSON.stringify(registroUsuario);
+        window.location.replace("home.html");
+      })
+      .catch((err) => {});
+  });
 
-        auth
-            .signInWithEmailAndPassword(usuarioInit, passwordInit)
-            .then(userCredential => {
-                formularioInicio.reset();
-                alert("Ingresado con exito!");
-                window.location.replace("home.html");
-            })
-            .catch(err => {
-                formularioInicio.reset();
-                alert("Usuario o contraseña incorrecta"); 
-            })
+  const formularioInicio = document.querySelector("#idFormularioCreado");
+  formularioInicio.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const usuarioInit = document.querySelector("#idUsuarioCreado").value;
+    const passwordInit = document.querySelector("#idPasswordCreado").value;
 
-    })
+    auth
+      .signInWithEmailAndPassword(usuarioInit, passwordInit)
+      .then((userCredential) => {
+        localStorage.usuario = JSON.stringify(usuarioInit);
+        formularioInicio.reset();
+        alert("Ingresado con exito!");
+        window.location.replace("home.html");
+      })
+      .catch((err) => {
+        formularioInicio.reset();
+        alert("Usuario o contraseña incorrecta");
+      });
+  });
 }
