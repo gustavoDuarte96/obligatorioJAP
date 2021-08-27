558af3fd4f5462a4410aa7ff4a959c40600f6fed
gustavoDuarte96 <gd245149@fi365.ort.edu.uy>
2021-08-27 10:23:34 -0300

    Entrega 2

--- js/products.js
+++ js/products.js
@@ -1,5 +1,96 @@
 window.addEventListener("load", inicio);
+
 function inicio(){
+    imprimir();
+    document.getElementById("rangeFilterCount").addEventListener("click", imprimirFiltrado);
+    document.getElementById("clearRangeFilter").addEventListener("click", resetear);
+}
+document.getElementById("idLabelAscendente").addEventListener("click", imprimir);
+document.getElementById("idLabelDescendente").addEventListener("click", imprimir);
+
+function imprimir(){
+    let personita = JSON.parse(localStorage.usuario);
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        
+        if (resultObj.status === "ok")
+        {
+            
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            /*let min = Number.MAX_VALUE;
+            for(let auto of categoriesArray){
+                if(auto.cost < min){
+                    min = auto.cost
+                }
+            }
+            document.getElementById("rangeFilterCountMin").value = min;
+            */
+            if(document.getElementById("idAscendente").checked){
+                alert("Ascendente");
+                categoriesArray.sort(function (a, b){
+                    if(a.cost > b.cost){
+                        return 1;
+                    }
+                    if(a.cost < b.cost){
+                        return -1;
+                    }
+                    return 0;
+                });
+            }
+            if(document.getElementById("idDescendente").checked){
+                alert("Descendente");
+                categoriesArray.sort(function (a, b){
+                    if(a.cost < b.cost){
+                        return 1;
+                    }
+                    if(a.cost > b.cost){
+                        return -1;
+                    }
+                    return 0;
+                });
+            }
+
+            showCategoriesList(categoriesArray);
+            
+            
+        }
+        
+    });   
+}
+
+function descendente(lista){
+    lista.sort(function (a, b){
+        if(a.cost < b.cost){
+            return 1;
+        }
+        if(a.cost > b.cost){
+            return -1;
+        }
+        return 0;
+    });
+}
+
+function ascendente(lista){
+    lista.sort(function (a, b){
+        if(a.cost > b.cost){
+            return 1;
+        }
+        if(a.cost < b.cost){
+            return -1;
+        }
+        return 0;
+    });
+}
+
+function resetear(){
+    document.getElementById("rangeFilterCountMin").value = "";
+    document.getElementById("rangeFilterCountMax").value = "";
+    document.getElementById("rangeFilterCountMin").placeholder = "min.";
+    document.getElementById("rangeFilterCountMax").placeholder = "máx.";
+    imprimir();
+}
+
+function imprimirFiltrado(){
     let personita = JSON.parse(localStorage.usuario);
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         
@@ -8,24 +99,29 @@ function inicio(){
 
             categoriesArray = resultObj.data;
             //Muestro las categorías ordenadas
-            
-            showCategoriesList(categoriesArray);
-            
-            
-        }
-        
-    });
-    const ordenarD = document.querySelector('#');
-    googleSesion.addEventListener('click', e => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider)
-            .then(result => {
-                window.location.replace("index2.html");
-            })
-            .catch(err => {
-            })
-    })
+            let arrayFiltrado = ordenarPorPrecio2(categoriesArray);
+            showCategoriesList(arrayFiltrado);
     
+        }      
+    }); 
+}
+
+function ordenarPorPrecio2(array){
+    let aux = 0;
+    let precioMinimo = document.getElementById("rangeFilterCountMin").value;
+    let precioMaximo = document.getElementById("rangeFilterCountMax").value;
+    if(precioMinimo > precioMaximo){
+        aux = precioMinimo;
+        precioMinimo = precioMaximo;
+        precioMaximo = aux;
+    }
+    let listaFiltrada = [];
+    for(let auto of array){
+        if(auto.cost >= precioMinimo && auto.cost <= precioMaximo){
+            listaFiltrada.push(auto);
+        }
+    }
+    return listaFiltrada;
 }
 
 function showCategoriesList(array){
@@ -42,3 +138,16 @@ function showCategoriesList(array){
     }
     document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
 }
+
+
+/* 
+lista.sort(function (a, b){
+            if(a.nombre.value() > b.nombre.toLowerCase()){
+                return 1;
+            }
+            if(a.nombre.toLowerCase() < b.nombre.toLowerCase()){
+                return -1;
+            }
+            return 0;
+        });
+*/
